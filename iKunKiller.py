0fb430a1041293f838e4a31901a81d6286a08779
RealCorebb <33654314+RealCorebb@users.noreply.github.com>
2023-07-09 23:36:33 +0800

    Working

--- iKunKiller.py
+++ iKunKiller.py
@@ -2,10 +2,10 @@ import threading
 import time
 import requests
 import keyboard
-from rcon import Client
-server_address = "mcadmin.bbrealm.com"  # 替换为Minecraft服务器的IP地址
+from mcrcon import MCRcon
+server_address = "192.168.2.125"  # 替换为Minecraft服务器的IP地址
 server_port = 25575  # 替换为Minecraft服务器RCON端口号
-rcon_password = "nishisola"  # 替换为你实际设置的RCON密码
+rcon_password = "asd951753"  # 替换为你实际设置的RCON密码
 
 # 数字形状定义
 digit_shapes = {
@@ -85,7 +85,8 @@ client = None
 
 def connect_to_server(server_address, server_port, rcon_password):
     global client
-    client = Client(server_address, server_port, rcon_password)
+    client = MCRcon(server_address, rcon_password)
+    client.connect()
 
 def get_likes(video_id):
     url = f"https://api.bilibili.com/x/web-interface/wbi/view?aid={video_id}"
@@ -95,34 +96,35 @@ def get_likes(video_id):
     return likes
 
 def update_likes_on_minecraft_server(likes):
-    # 清空之前的数字方块
-    likes = int(likes)
-    client.run(f"/fill {x1} {y1} {z1} {x2} {y2} {z2} minecraft:air")
 
     # 摆放新的数字方块
-    x, y, z = x1, y1, z1 = 100, 64, 100  # 替换为数字方块的起始位置坐标
+    x, y, z = x1, y1, z1 = 2069, 81, -2015  # 替换为数字方块的起始位置坐标
 
-    x2 = x1 + 10
-    y2 = y1 + 10
+    x2 = x1 - 30
+    y2 = y1 - 7
     z2 = z1
 
+    # 清空之前的数字方块
+    likes = str(likes)
+    client.command(f"fill {x1} {y1} {z1} {x2} {y2} {z2} minecraft:air")
+    print('更新方块数字:',likes)
     for digit in likes:
         if digit in digit_shapes:
             shape = digit_shapes[digit]
             for i, row in enumerate(shape):
                 for j, char in enumerate(row):
                     if char == '#':
-                        client.run(f"/setblock {x + j} {y + i} {z} minecraft:stone")
-            x += 4  # 数字之间的间距
+                        client.command(f"setblock {x - j} {y - i} {z} minecraft:ochre_froglight")
+            x -= 4  # 数字之间的间距
 
 def send_fireball(idx,length):
     if(idx == 0):
         keyboard.press('F8')
-    client.run("/summon minecraft:fireball ~ ~1 ~ {ExplosionPower:100,direction:[0.0,0.0,0.0]}")
+    client.command("summon minecraft:fireball 2074 64 -2048 {ExplosionPower:4,Motion:[1.0,0.0,0.0]}")
     if(idx == length - 1):
         threading.Timer(2, lambda: keyboard.press('F8')).start()
 
-#connect_to_server(server_address, server_port, rcon_password)
+connect_to_server(server_address, server_port, rcon_password)
 video_id = 857470696  # 替换为你要获取点赞数的视频ID
 likes = None
 def watch_likes():
@@ -133,14 +135,12 @@ def watch_likes():
         if (new_likes != likes):
             if(likes != None):
                 print(f"触发 {abs(new_likes - likes)} 次")
-                update_likes_on_minecraft_server(likes)
+                update_likes_on_minecraft_server(new_likes)
                 for i in range(abs(new_likes - likes)):
-                    send_fireball(i,abs(range(new_likes - likes)))
+                    send_fireball(i,abs(new_likes - likes))
             likes = new_likes
             print('新点赞')
         time.sleep(1)
 
 if __name__ == '__main__':
-    for i in range(1):
-        print(i)
-    #watch_likes()
+    watch_likes()
